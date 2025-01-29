c21b017f307332bf3b749bf8e056f1380e81b831
Guillaume Fgt <guillaume.faguet@protonmail.com>
2025-01-29 12:49:02 +0100

    2020 day5 part 2 solved.

--- src/bin/day5.rs
+++ src/bin/day5.rs
@@ -3,18 +3,38 @@ use std::cmp::{max, min};
 
 use aoc_rust::inputs;
 
+// //part1
+// fn main()->inputs::Result<()> {
+//     let input=inputs::fetch(2020, 5)?;
+//     let mut sum=0;
+
+//     for (instr_row,instr_col) in parse(&input) {
+//         let row=compute_place(instr_row,0,127,'F');
+//         let col=compute_place(instr_col, 0, 7,'L');
+//         sum=max(sum,row*8+col);
+//     }
+//     print!("{sum}");
+//     Ok(())
+// }
+
+//part2
 fn main()->inputs::Result<()> {
     let input=inputs::fetch(2020, 5)?;
-    let mut sum=0;
+    let mut seat_ids=Vec::new();
 
     for (instr_row,instr_col) in parse(&input) {
         let row=compute_place(instr_row,0,127,'F');
         let col=compute_place(instr_col, 0, 7,'L');
-        sum=max(sum,row*8+col);
+        seat_ids.push(row*8+col);
     }
-    print!("{sum}");
+    for i in 0..890 {
+        if !seat_ids.contains(&i) {
+            println!("{i}");
+        }
+    }
     Ok(())
 }
+
 
 fn compute_place(instr:&str,left:i32,right:i32,lett:char)->i32 {
     let mut l:i32=left;

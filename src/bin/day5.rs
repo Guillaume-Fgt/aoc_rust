f2c0e2894040fe36f80509bc91a5902b0b45d94e
Guillaume Fgt <guillaume.faguet@protonmail.com>
2025-01-29 12:18:21 +0100

    2020 day5 part 1 solved.

---
+++ src/bin/day5.rs
@@ -0,0 +1,62 @@
+
+use std::cmp::{max, min};
+
+use aoc_rust::inputs;
+
+fn main()->inputs::Result<()> {
+    let input=inputs::fetch(2020, 5)?;
+    let mut sum=0;
+
+    for (instr_row,instr_col) in parse(&input) {
+        let row=compute_place(instr_row,0,127,'F');
+        let col=compute_place(instr_col, 0, 7,'L');
+        sum=max(sum,row*8+col);
+    }
+    print!("{sum}");
+    Ok(())
+}
+
+fn compute_place(instr:&str,left:i32,right:i32,lett:char)->i32 {
+    let mut l:i32=left;
+    let mut r:i32=right;
+
+    for letter in instr.chars() {
+        if letter==lett {
+            if r-l==1 {
+                return min(l,r);
+            }
+            r=(r-l).div_euclid(2)+l;
+        }
+        else {
+            if r-l==1 {
+                return max(l,r);
+            }
+            l=(r-l).div_euclid(2)+1+l;
+        }
+    }
+    0
+}
+
+fn parse(input:&str)-> impl Iterator<Item = (&str, &str)>{
+    input.split("\n").map(|e|e.split_at(7))
+}
+
+#[test]
+fn parse_input() {
+    let mut result=parse(
+        "FFBBFFFLLL
+                BFBFBFBLLR");
+    assert_eq!(result.next(),Some(("FFBBFFF","LLL")));
+}
+
+#[test]
+fn row() {
+    let row=compute_place("FBFBBFF",0,127,'F');
+    assert_eq!(row,44);
+}
+
+#[test]
+fn col() {
+    let row=compute_place("RLR",0,7,'L');
+    assert_eq!(row,5);
+}

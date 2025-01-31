47fbf22c41cc51e8c17bea8ec536510338cb5ddf
Guillaume Fgt <guillaume.faguet@protonmail.com>
2025-01-31 11:11:10 +0100

    2020 day6 part2 solved.

--- src/bin/day6.rs
+++ src/bin/day6.rs
@@ -2,17 +2,38 @@ use std::collections::HashSet;
 
 use aoc_rust::inputs;
 
+// //part1
+// fn main()->inputs::Result<()> {
+//     let input=inputs::fetch(2020, 6)?;
+//     let mut total=0;
+//     for answer in input.split("\n\n") {
+//         let answers=answer.split("\n").map(|a|a.chars());
+//         let mut set=HashSet::new();
+//         for a in answers {
+//             set.extend(a);
+//         }
+//         total+=set.len();
+//     }
+//     print!("Total: {}",total);
+//     Ok(())
+// }
+
+
+//part2
 fn main()->inputs::Result<()> {
     let input=inputs::fetch(2020, 6)?;
-    let mut total=0;
+    let mut sum=0;
+
     for answer in input.split("\n\n") {
-        let answers=answer.split("\n").map(|a|a.chars());
-        let mut set=HashSet::new();
-        for a in answers {
-            set.extend(a);
+        let mut group_a:Vec<HashSet<char>>=Vec::new();
+        for a in answer.split("\n") {
+            let mut set = HashSet::new();
+            set.extend(a.chars());
+            group_a.push(set);
         }
-        total+=set.len();
+        let total=group_a.into_iter().reduce(|a,b|a.intersection(&b).cloned().collect()).ok_or("error")?;
+        sum+=total.len();
     }
-    print!("Total: {}",total);
+    print!("{sum}");
     Ok(())
 }

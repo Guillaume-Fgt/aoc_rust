06df7d2184e6dd97dec35ee0ec70a89d65704b49
Guillaume Fgt <guillaume.faguet@protonmail.com>
2025-02-03 21:43:18 +0100

    2020 day7 part2 solved!

--- src/bin/day7.rs
+++ src/bin/day7.rs
@@ -3,6 +3,39 @@ use std::collections::{HashMap, HashSet};
 
 use aoc_rust::inputs;
 use regex::Regex;
+
+//part1
+// fn main()->inputs::Result<()> {
+//     let input=inputs::fetch(2020, 7)?;
+//     let re=Regex::new(r"(?:(\d)\s)(\w+\s\w+) bags?").unwrap();
+//     let mut bag_types:HashMap<&str,HashMap<&str,u32>>=HashMap::new();
+//     for line in input.lines() {
+//         let bag:Vec<&str>=line.splitn(2, " bags").collect();
+//         let entry=bag_types.entry(bag[0]).or_insert(HashMap::new());
+//         for (_,[num,name]) in re.captures_iter(line).map(|caps|caps.extract()) {
+//             entry.insert(name,num.trim().parse::<u32>().unwrap());
+//             }
+//     }
+
+//     let mut queue=Vec::new();
+//     let mut valid:HashSet<&str>=HashSet::new();
+//     queue.push("shiny gold");
+
+//     while !queue.is_empty() {
+//         let current=queue.pop().unwrap();
+//         for (k,v) in bag_types.iter() {
+//             if v.contains_key(&current) {
+//                 queue.push(k);
+//                 valid.insert(k);
+//             }
+//         }
+//     }
+//     println!("{:?}",valid);
+//     println!("{:?}",valid.len());
+//     Ok(())
+// }
+
+//part2
 fn main()->inputs::Result<()> {
     let input=inputs::fetch(2020, 7)?;
     let re=Regex::new(r"(?:(\d)\s)(\w+\s\w+) bags?").unwrap();
@@ -15,21 +48,21 @@ fn main()->inputs::Result<()> {
             }
     }
 
-    let mut queue=Vec::new();
-    let mut valid:HashSet<&str>=HashSet::new();
-    queue.push("shiny gold");
+    let mut queue: Vec<(&str,u32)>=Vec::new();
+    let mut total=0;
+    queue.push(("shiny gold",1));
 
     while !queue.is_empty() {
-        let current=queue.pop().unwrap();
-        for (k,v) in bag_types.iter() {
-            if v.contains_key(&current) {
-                queue.push(k);
-                valid.insert(k);
+        println!("{:?}",queue);
+        println!("{:?}",total);
+        let (current,quant)=queue.pop().unwrap();
+        for (k,v) in &bag_types[current] {
+            total+=quant*v;
+            queue.push((k,quant*v));
             }
-        }
+        
     }
-    println!("{:?}",valid);
-    println!("{:?}",valid.len());
+    println!("{:?}",total);
     Ok(())
 }
 

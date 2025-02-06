f5e8955db696c3dfec3f16909a7446e8e95e6339
Guillaume Fgt <guillaume.faguet@protonmail.com>
2025-02-06 09:05:12 +0100

    2020 day8 part2 solved.

--- src/bin/day8.rs
+++ src/bin/day8.rs
@@ -1,46 +1,92 @@
 use aoc_rust::inputs;
 
+//part1
+// fn main()->inputs::Result<()> {
+//     let input=inputs::fetch(2020, 8)?;
+//     let mut instr=Vec::new();
+//     let mut total=0;
+//     for (inst,num) in input.lines().map(|l|l.split_once(" ").unwrap()) {
+//         instr.push((inst,num.parse::<i32>().unwrap())); 
+        
+//     }
+    
+//     let mut curr_index:i32=0;
+//     let mut visited=Vec::new();
+
+
+//     loop {
+//         let current=instr.get(curr_index as usize).unwrap();
+//         if visited.contains(&curr_index) {
+//             break;
+//         }
+//         visited.push(curr_index);
+//         println!("{:?}",visited);
+//         match current.0 {
+//             "acc"=>{
+//                 total+=current.1;
+//                 curr_index+=1;
+//             }
+//             "jmp"=>{
+//                 curr_index+=current.1 as i32;
+//             }
+//             "nop"=>{
+//                 curr_index+=1;
+//             }
+//             _=>{}
+//         }
+//     }
+//     println!("{total}");
+//     Ok(())
+// }
+
+//part2
 fn main()->inputs::Result<()> {
     let input=inputs::fetch(2020, 8)?;
     let mut instr=Vec::new();
-    let mut total=0;
     for (inst,num) in input.lines().map(|l|l.split_once(" ").unwrap()) {
         instr.push((inst,num.parse::<i32>().unwrap())); 
         
     }
-    
-    let mut curr_index:i32=0;
-    let mut visited=Vec::new();
+    let mut visited:Vec<(i32,bool)>=Vec::new();
+    let mut queue:Vec<(i32,i32,bool)>=Vec::new();
+    queue.push((0,0,false));
+    let goal=instr.len() as i32;
 
-
-    loop {
-        let current=instr.get(curr_index as usize).unwrap();
-        if visited.contains(&curr_index) {
+    while !queue.is_empty() {
+        println!("{:?}",queue);
+        let (curr_index,mut total,already_changed):(i32,i32,bool)=queue.pop().unwrap();
+        println!("{curr_index}{total}");
+        
+        if curr_index==goal {
+            println!("Success!");
+            println!("The total is: {total}");
             break;
         }
-        visited.push(curr_index);
-        println!("{:?}",visited);
+        let current=instr.get(curr_index as usize).unwrap();
+        if visited.contains(&(curr_index,already_changed)) {
+            continue;
+        }
+        visited.push((curr_index,already_changed));
         match current.0 {
             "acc"=>{
                 total+=current.1;
-                curr_index+=1;
+                queue.push((curr_index+1,total,already_changed));
             }
             "jmp"=>{
-                curr_index+=current.1 as i32;
+                queue.push((curr_index+current.1 as i32,total,already_changed));
+                if !already_changed {
+                    queue.push((curr_index+1,total,true));
+                }
             }
             "nop"=>{
-                curr_index+=1;
-            }
-            "viewed"=>{
-                break;
+                queue.push((curr_index+1,total,already_changed));
+                if !already_changed {
+                    queue.push((curr_index+current.1 as i32,total,true));
+                } 
             }
             _=>{}
         }
     }
-    println!("{total}");
     Ok(())
 }
-
-
-
 

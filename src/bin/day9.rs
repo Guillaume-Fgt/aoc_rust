9987f50d64adda69553729370d9f6c7892fbb456
Guillaume Fgt <guillaume.faguet@protonmail.com>
2025-02-10 15:17:37 +0100

    2020 day9 part2 solved.

--- src/bin/day9.rs
+++ src/bin/day9.rs
@@ -1,5 +1,36 @@
 use aoc_rust::inputs;
-use std::collections::HashSet;
+
+//part1
+
+// fn main() -> inputs::Result<()> {
+//     let input = inputs::fetch(2020, 9)?;
+//     let input = input
+//         .lines()
+//         .map(|n| n.parse::<i64>().unwrap())
+//         .collect::<Vec<i64>>();
+
+//     let mut start = 0;
+//     let mut end = 25;
+
+//     while end <= input.len() - 1 {
+//         let nums = &input[start..end];
+//         let curr = &input[end];
+//         let valid = nums
+//             .into_iter()
+//             .any(|e| nums.contains(&(curr - e)) && curr - e != *e);
+//         if !valid {
+//             println!("{curr}");
+//             break;
+//         }
+//         start += 1;
+//         end += 1;
+//     }
+
+//     Ok(())
+// }
+
+//part2
+
 fn main() -> inputs::Result<()> {
     let input = inputs::fetch(2020, 9)?;
     let input = input
@@ -7,22 +38,33 @@ fn main() -> inputs::Result<()> {
         .map(|n| n.parse::<i64>().unwrap())
         .collect::<Vec<i64>>();
 
-    let mut start = 0;
-    let mut end = 25;
+    let invalid_num=1038347917;
+    let mut sum:i64=0;
+    let mut origin_index=0;
+    let mut index=0;
+    let mut nums:Vec<i64>=Vec::new();
 
-    while end <= input.len() - 1 {
-        let nums = &input[start..end];
-        let curr = &input[end];
-        let valid = nums
-            .into_iter()
-            .any(|e| nums.contains(&(curr - e)) && curr - e != *e);
-        if !valid {
-            println!("{curr}");
+    loop {
+        let num=input.get(index).unwrap();
+        sum+=num;
+        nums.push(*num);
+        if sum==invalid_num {
+            println!("solved!");
+            println!("{:?}",nums.iter().min());
+            println!("{:?}",nums.iter().max());
+            println!("{:?}",nums.iter().min().unwrap()+nums.iter().max().unwrap());
             break;
         }
-        start += 1;
-        end += 1;
+        if sum>invalid_num {
+            origin_index+=1;
+            index=origin_index;
+            sum=0;
+            nums.clear();
+            continue;
+        }
+        index+=1;
     }
+
 
     Ok(())
 }
